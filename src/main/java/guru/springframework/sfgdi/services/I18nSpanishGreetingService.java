1b3de601f788f2cc6a09abc81ed3714beb37cc6a
Mike <falchek@gmail.com>
2020-08-01 16:45:38 -0400

    profiles completed

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
  * Note - the qualifier for this service has the same name as the English Controller
  */
 @Service("i18nService")
-@Profile("ES")
+@Profile({"ES", "default"})
 public class I18nSpanishGreetingService implements GreetingService{
 
     @Override
