b2e0f489541effcc34873f51dfac16f7fccddc30
Mike <falchek@gmail.com>
2020-08-01 17:41:47 -0400

    Swithcing to pet clinic app

---
+++ src/main/java/guru/springframework/sfgdi/services/LifeCycleDemoBean.java
@@ -0,0 +1,62 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * This class demonstrates all of the bean lifecycle items
+ */
+@Component
+public class LifeCycleDemoBean implements InitializingBean, DisposableBean, BeanNameAware, BeanFactoryAware, ApplicationContextAware {
+
+
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("## The Lifecycle bean has been terminated");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("## Bean Factory has been set");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("## Bean Factory has been set");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("## Application context has been set");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("## My Bean Name is " + name);
+    }
+
+    @PostConstruct
+    public void postContrust() {
+        System.out.println("## The Post Construct annotated method has been called. ");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("##  The Pre Destroy annotation method has been called");
+    }
+
+    public void beforeInit() {
+        System.out.println("## - Before init - Called by Bean Post Processor");
+    }
+
+    public void afterInit() {
+        System.out.println("## - After Init - Called by Bean Post Processor");
+    }
+}
